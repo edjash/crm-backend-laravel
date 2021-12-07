a165b634f73244beada61a50bbe3d4b61918d58c
Ed Shortt <dev@edshortt.co.uk>
2021-12-07 11:33:57 +0000

    Added demo user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CountriesSeeder::class,
             ContactsSeeder::class,
-           // CompaniesSeeder::class
+            UsersSeeder::class
+            // CompaniesSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
     }
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'email' => 'demo@eddemocrm.com',
+            'password' => Hash::make('demo123'),
+            'email_verified_at' => now(),
+        ]);
+    }
+}
