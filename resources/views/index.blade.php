1524a6e04ed7bbf65ffd133eb130e2befb7b2e79
Ed Shortt <edjash@gmail.com>
2022-03-02 10:53:22 +0000

    Updated web route for cra

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,11 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="robots" content="noindex,nofollow">
-    <link rel="icon" type="image/svg+xml" href="/js/frontend/{{ $favicon }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>CRM Demo</title>
-    <script type="module" crossorigin src="/frontend/{{ $index_js }}"></script>
-    <link rel="modulepreload" href="/frontend/{{ $vendor_js }}">
 </head>
-
+    <script defer="defer" src="{{$main_js}}"></script>
+</head>
 <body>
     <div id="root"></div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,41 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\File;
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-*/
+ */
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/forgot-password', [PasswordResetController::class, 'index']);
 
 Route::get('/{any?}', function () {
-    die("OK");
+
     $data = [
-        "index_js" => "",
+        "main_js" => "",
         "vendor_js" => "",
-        "favicon" => ""
+        "favicon" => "",
     ];
 
-    $files = File::files(public_path('frontend'));
+    $files = File::files(public_path('static/js'));
 
     foreach ($files as $file) {
         $fname = $file->getFilename();
-
-        if (preg_match('/index\.(.*)\.js/', $fname)) {
-            $data['index_js'] = $fname;
+        $ext = substr(strrchr($fname, '.'), 1);
+        if ((strpos($fname, 'main.') === 0) && ($ext === 'js')) {
+            $data['main_js'] = '/static/js/' . $fname;
             continue;
         }
-        if (preg_match('/vendor\.(.*)\.js/', $fname)) {
-            $data['vendor_js'] = $fname;
-            continue;
-        }
-        if (preg_match('/favicon\.(.*)\.svg/', $fname)) {
-            $data['favicon'] = $fname;
-            continue;
-        }
+        /*
+    if (preg_match('/favicon\.(.*)\.svg/', $fname)) {
+    $data['favicon'] = $fname;
+    continue;
+    }*/
     }
 
     return view('index', $data);
