041e5c8ddc038bb5b940f254fa271801dc5b9af6
Ed Shortt <ed.shortt@gmail.com>
2022-06-15 09:07:32 +0100

    Fix index template missing

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CRM Demo</title>
+</head>
+<script defer="defer" src="{{ $main_js }}"></script>
+</head>
+
+<body>
+    <div id="root"></div>
+
+</body>
+
+</html>
